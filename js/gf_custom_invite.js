0d72702d244dd7c3d34161a2ac66519631c27467
dalay <dalay@drupalka.ru>
2018-12-11 23:37:45 +0300

    change novelty link in js

--- js/gf_custom_invite.js
+++ js/gf_custom_invite.js
@@ -2,7 +2,7 @@
     Drupal.behaviors.gfCustomInvite = {
         attach: function (context, settings) {
             // в основном меню меняем ссылку каталога на ссылку страницы новинок.
-            $('.nav-main a[href="/ru/shop"]', context).attr("href", '/ru/novelty/2764');
+            $('.nav-main a[href="/ru/shop"]', context).attr("href", '/ru/novelty/all');
         }
     };
 })(jQuery);
